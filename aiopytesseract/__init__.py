875ee9e240bf06e370615111f00afd2f1dfb144c
alexandre menezes <alexandre.fmenezes@gmail.com>
2022-04-20 21:58:11 -0300

    fix cattr error

--- aiopytesseract/__init__.py
+++ aiopytesseract/__init__.py
@@ -1,19 +1,8 @@
-from .commands import (
-    confidence,
-    deskew,
-    get_languages,
-    get_tesseract_version,
-    image_to_boxes,
-    image_to_data,
-    image_to_hocr,
-    image_to_osd,
-    image_to_pdf,
-    image_to_string,
-    languages,
-    run,
-    tesseract_parameters,
-    tesseract_version,
-)
+from .commands import (confidence, deskew, get_languages,
+                       get_tesseract_version, image_to_boxes, image_to_data,
+                       image_to_hocr, image_to_osd, image_to_pdf,
+                       image_to_string, languages, run, tesseract_parameters,
+                       tesseract_version)
 
 __version__ = "0.6.0"
 __all__ = [
